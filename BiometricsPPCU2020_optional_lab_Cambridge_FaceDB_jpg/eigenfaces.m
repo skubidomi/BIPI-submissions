8f759ab346dcb3586415d72f3d20b7ae6a4315f8
Domonkos Skublics <skubidomi@gmail.com>
2020-11-10 22:37:34 +0100

    optional lab last is missing

--- BiometricsPPCU2020_optional_lab_Cambridge_FaceDB_jpg/eigenfaces.m
+++ BiometricsPPCU2020_optional_lab_Cambridge_FaceDB_jpg/eigenfaces.m
@@ -34,9 +34,46 @@ Uraw = A*V;
 U = Uraw./vecnorm(Uraw);
 
 % 6.4
-K = 40;
+K = 320;
 U = U(:,1:K);
 
+% 7
+Y = transpose(U)*A;
+
+% 8
+Phi = test_img - Psi;
+y = transpose(U)*Phi;
+
+% 9.1
+Diff = zeros(1,size(Y,2));
+for i = 1:size(Y,2)
+    Diff(i) = norm(Y(:,i)-y);
+end
+
+% 9.2
+[minimum_value, minimum_index] = min(Diff);
+
+% 10
+Phi_rec = U*Y(:,minimum_index);
+recognised_test_img = Phi_rec + Psi;
+
+% 11
+figure(1);
+subplot(1,3,1);
+imshow(reshape(uint8(test_img),112,92));
+subplot(1,3,2);
+imshow(reshape(uint8(Psi),112,92));
+subplot(1,3,3);
+imshow(reshape(uint8(recognised_test_img),112,92));
+
+%12
+figure(2);
+for i = 1:10
+    subplot(2,5,i);
+    Uimg = U-
+    Uimg = (U(:,i)/max(U(:,i)))*256;
+    imshow(reshape(uint8(Uimg),112,92));
+end
 
 
 
